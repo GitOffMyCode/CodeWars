78e4080513672f473d06031714d97eebcc2d0699
Genevieve Heald <gennaheald@outlook.com>
2019-11-29 10:10:08 +0000

    refactored consecutiveLetters to use String.prototype.includes()

--- challenges/consecutiveLetters.js
+++ challenges/consecutiveLetters.js
@@ -1,14 +1,9 @@
 // In this Kata, we will check if a string contains consecutive letters as they appear in the English alphabet and if each letter occurs only once.
 
 const consecutiveLetters = s => {
-    let codes = [];
-    for (let i = 0; i < s.length; i++) codes.push(s[i].charCodeAt(0));
-    codes = codes.sort((a,b)=>a-b);
-    const codeStart = codes[0];
-    const codeLength = codes.length;
-    const codeEnd = codes[codeLength-1];
-    if (codeEnd === codeStart + codeLength -1) return true;
-    else return false;
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    const sSorted = [...s].sort().join('');
+    return alphabet.includes(sSorted);
 }
 
 module.exports = consecutiveLetters;
