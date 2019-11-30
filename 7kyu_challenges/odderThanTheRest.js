e212c7d076f8e43702a8765e2657baa17d2f97bc
Genevieve Heald <gennaheald@outlook.com>
2019-11-30 22:15:50 +0000

    Odder Than the Rest challenge added

---
+++ 7kyu_challenges/odderThanTheRest.js
@@ -0,0 +1,8 @@
+// Codewars - Odder Than the Rest
+// Create a method that takes an array/list as an input, and outputs the index at which the sole odd number is located. This method should work with arrays with negative numbers. If there are no odd numbers in the array, then the method should output -1.
+
+function oddOne(arr) {
+
+}
+
+module.exports = oddOne;
---
+++ 7kyu_tests/odderThanTheRest.test.js
@@ -0,0 +1,20 @@
+const oddOne = require("../7kyu_challenges/odderThanTheRest");
+
+describe("oddOne", () => {
+
+    test("returns the index at which the sole odd number is located", () => {
+        expect(oddOne([2])).toBe(0); 
+        expect(oddOne([2,4,6,7,10])).toBe(3);    
+        expect(oddOne([2,16,98,10,13,78])).toBe(4);
+        expect(oddOne([4,-8,98,-12,-7,90,100])).toBe(4);
+        expect(oddOne([1009872,1009873])).toBe(1);
+        expect(oddOne([1,2])).toBe(0); 
+    });
+
+    test("returns -1 if no odd number is found", () => {
+        expect(oddOne([])).toBe(-1);
+        expect(oddOne([2,4,6,8])).toBe(-1);
+        expect(oddOne([246, 870, 990, 120, 7652])).toBe(-1);
+    });
+
+});
