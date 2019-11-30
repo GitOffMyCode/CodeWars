1c522d5d2eb10f2f2a66162385ea269814db1a2d
Genevieve Heald <gennaheald@outlook.com>
2019-11-30 16:56:58 +0000

    shortestWord challange completed

--- 7kyu_challenges/shortestWord.js
+++ 7kyu_challenges/shortestWord.js
@@ -2,8 +2,12 @@
 
 // Simple, given a string of words, return the length of the shortest word(s). String will never be empty and you do not need to account for different data types.
 
-function shortestWord(s){
-    return s
+function shortestWord(s) {
+    s = s.split(' ').sort((a, b) => a.length - b.length);
+    return s[0].length
 }
+
+shortestWord("do bees have knees")
+shortestWord("ashdfiawuhegbjbgkaehgk a")
 
 module.exports = shortestWord;
--- 7kyu_tests/shortestWord.test.js
+++ 7kyu_tests/shortestWord.test.js
@@ -2,8 +2,10 @@ const shortestWord = require("../7kyu_challenges/shortestWord");
 
 describe("shortestWord", () => {
 
-    test("description of test", () => {
-        expect(shortestWord()).toBe(true);    
+    test("returns the length of the shortest word(s) in a string", () => {
+        expect(shortestWord("do bees have knees")).toBe(2);
+        expect(shortestWord("to be or not to be")).toBe(2);
+        expect(shortestWord("Supercalifragilisticexpialidocious")).toBe(34); expect(shortestWord("Supercalifragilisticexpialidocious is a very long word")).toBe(1);
     });
 
 });
