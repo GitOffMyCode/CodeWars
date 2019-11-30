6e39a26f0752f111fe688f83f149169c936b8048
Genevieve Heald <gennaheald@outlook.com>
2019-11-30 21:53:59 +0000

    growthOfPopulation challenge completed

--- 7kyu_challenges/growthOfPopulation.js
+++ 7kyu_challenges/growthOfPopulation.js
@@ -3,8 +3,19 @@
 // In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater or equal to p = 1200 inhabitants? 
 // Return n number of years.
 
+// p0 = population at beginning of year (1000)
+// percent = 2 (0.02)
+// aug = inhabitants coming or leaving each year (50)
+// p = population target (1200)
+
 function nbYear(p0, percent, aug, p) {
-    // your code
+    let popCounter = p0;
+    let years = 0;
+    for (let i = 0; popCounter < p; i++) {
+        popCounter = popCounter + (popCounter * percent / 100) + aug;
+        years++
+    }
+    return years;
 }
 
 module.exports = nbYear;
--- 7kyu_tests/growthOfPopulation.test.js
+++ 7kyu_tests/growthOfPopulation.test.js
@@ -4,6 +4,9 @@ describe("nbYear", () => {
 
     test("returns the number of years required to grow population to p", () => {
         expect(nbYear(1500, 5, 100, 5000)).toBe(15);
+        expect(nbYear(1500000, 2.5, 10000, 2000000)).toBe(10);
+        expect(nbYear(1500000, 0.25, 1000, 2000000)).toBe(94);
+        expect(nbYear(1500000, 0.75, 50, 1500000)).toBe(0);
     });
 
 });
