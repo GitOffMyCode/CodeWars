6f3a83eaec72f2cedca37361bd339ce2ea7b09e9
Genevieve Heald <gennaheald@outlook.com>
2019-11-30 23:03:10 +0000

    mapToDouble challenge completed

--- 7kyu_challenges/mapToDouble.js
+++ 7kyu_challenges/mapToDouble.js
@@ -1,8 +1,6 @@
 // Codewars - Use map() to double the values in an array
 // Make the double() function return a new array with each value twice as large as the corresponding value in the passed in array. Your solution must use the map() function of the built-in javascript Array object. If you're not familiar with map(), reading over the documentation may help.
 
-function double(array) {
-
-}
+const double = array => array.map(num => num * 2);
 
 module.exports = double;
--- 7kyu_tests/mapToDouble.test.js
+++ 7kyu_tests/mapToDouble.test.js
@@ -4,6 +4,14 @@ describe("double", () => {
 
     test("return a new array with each value doubled in size", () => {
         expect(double([2,4,6,8,10])).toEqual([4,8,12,16,20]); 
+        expect(double([1,45678])).toEqual([2,91356]);
+        expect(double([100, 100, 100, 100, 100])).toEqual([200, 200, 200, 200, 200]); 
+        expect(double([5000])).toEqual([10000]); 
+    });
+
+    test("works with negative numbers", () => {
+        expect(double([71,-548,12.3,31415])).toEqual([142,-1096,24.6,62830]); 
+        expect(double([-5000])).toEqual([-10000]);
     });
 
 });
