d45552af293b7622a731a44f40e22e41b9b89294
Genevieve Heald <gennaheald@outlook.com>
2019-11-30 13:31:17 +0000

    validatePin challenge completed

--- 7kyu_tests/validatePin.test.js
+++ 7kyu_tests/validatePin.test.js
@@ -2,9 +2,16 @@ const validatePin = require("../7kyu_challenges/validatePin");
 
 describe("validatePin", () => {
 
-    test("returns true if PIN contains exactly 4 digits or exactly 6 digits", () => {
+    test("returns true if PIN contains exactly 4 or 6 digits", () => {
         expect(validatePin(1234)).toBe(true);    
         expect(validatePin(567890)).toBe(true);  
+    });
+
+    test("returns false if PIN does not contain exactly 4 or 6 digits", () => {
+        expect(validatePin()).toBe(false);  
+        expect(validatePin(12345)).toBe(false);  
+        expect(validatePin(1234567)).toBe(false); 
+        expect(validatePin(12345678901234567890)).toBe(false); 
     });
 
 });
