4c94c8d633e6a49036f5ba4920083f7469769829
Genevieve Heald <gennaheald@outlook.com>
2019-11-30 13:46:35 +0000

    shortestWord challenge started

---
+++ 7kyu_challenges/shortestWord.js
@@ -0,0 +1,9 @@
+// Codewars - Shortest Word
+
+// Simple, given a string of words, return the length of the shortest word(s). String will never be empty and you do not need to account for different data types.
+
+function shortestWord(s){
+    return s
+}
+
+module.exports = shortestWord;
---
+++ 7kyu_tests/shortestWord.test.js
@@ -0,0 +1,9 @@
+const shortestWord = require("../7kyu_challenges/shortestWord");
+
+describe("shortestWord", () => {
+
+    test("description of test", () => {
+        expect(shortestWord()).toBe(true);    
+    });
+
+});
