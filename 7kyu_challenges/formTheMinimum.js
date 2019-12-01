e0234047647184221231cdace318b3016e5e1f48
Genevieve Heald <gennaheald@outlook.com>
2019-12-01 11:40:20 +0000

    formTheMinimum challenge completed

--- 7kyu_challenges/formTheMinimum.js
+++ 7kyu_challenges/formTheMinimum.js
@@ -1,8 +1,9 @@
 // Codewars - Form The Minimum
 // Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates). Only positive integers will be passed to the function (> 0 ), no negatives or zeros.
 
-function minValue(values){
-    //your code here
-  }
+const minValue = values => {
+    const newValues = new Set(values);
+    return Number([...newValues].sort().join(''));
+}
 
 module.exports = minValue;
--- 7kyu_tests/formTheMinimum.test.js
+++ 7kyu_tests/formTheMinimum.test.js
@@ -3,9 +3,12 @@ const minValue = require("../7kyu_challenges/formTheMinimum");
 describe("formTheMinimum", () => {
 
     test("returns the smallest number that can be formed from an array of positive intigers greater than 0", () => {
+        expect(minValue([1])).toBe(1);
         expect(minValue([1, 3, 1])).toBe(13);
         expect(minValue([4, 7, 5, 7])).toBe(457);
         expect(minValue([4, 8, 1, 4])).toBe(148);
+        expect(minValue([10, 10, 10, 10, 10])).toBe(10);
+        expect(minValue([999, 382, 999, 999])).toBe(382999);
         expect(minValue([3, 6, 5, 5, 9, 8, 7, 6, 3, 5, 9])).toBe(356789);
     });
 
