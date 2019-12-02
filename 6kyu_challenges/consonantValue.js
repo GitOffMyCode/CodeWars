211bafebea0472fe33abf41301e1430512404729
Genevieve Heald <gennaheald@outlook.com>
2019-12-02 22:17:36 +0000

    consonantValue challenge part completed

--- 6kyu_challenges/consonantValue.js
+++ 6kyu_challenges/consonantValue.js
@@ -1,10 +1,14 @@
 // Codewars - Consonant Value
 // Given a lowercase string that has alphabetic characters only and no spaces, return the highest value of consonant substrings. Consonants are any letters of the alpahabet except "aeiou".
 // We shall assign the following values: a = 1, b = 2, c = 3, .... z = 26.
-// For example, for the word "zodiacs", let's cross out the vowels. We get: "z o d ia cs"
+// For example, for the word "zodiacs", let's cross out the vowels. We get: "z, d, cs"
 
 function solve(s) {
-    return s;
+    const array = s.match(/[^aeiou]+/gi);
+    return array;
+    
 }
+
+console.log(solve("zodiacs")) //-> 26
 
 module.exports = solve;
--- 6kyu_tests/consonantValue.test.js
+++ 6kyu_tests/consonantValue.test.js
@@ -2,7 +2,7 @@ const solve = require("../6kyu_challenges/consonantValue");
 
 describe("solve", () => {
 
-    test("whatever here", () => {
+    test("returns the highest value of consonant substrings", () => {
         expect(solve("zodiac")).toBe(26);
         expect(solve(("catchphrase"))).toBe(73);
         expect(solve("chruschtschov")).toBe(80);
