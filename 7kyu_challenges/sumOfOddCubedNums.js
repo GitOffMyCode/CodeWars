049a4f7e92b1acb95acf9d728c316dc0c852f305
Genevieve Heald <gennaheald@outlook.com>
2019-12-02 21:27:55 +0000

    sumOfOddCubedNums completed

--- 7kyu_challenges/sumOfOddCubedNums.js
+++ 7kyu_challenges/sumOfOddCubedNums.js
@@ -1,8 +1,12 @@
 // Sum of Odd Cubed Numbers
-// Find the sum of the odd numbers within an array, after cubing the initial integers. The function should return undefined/None/nil/NULL if any of the values aren't numbers.
+// Find the sum of the odd numbers within an array, after cubing the initial integers. The function should return undefined if any of the values aren't numbers.
 
 function cubeOdd(arr) {
-
+    if (arr.every(el => typeof el === "number")) {
+        return arr.map(num => num * num * num).filter(num => num % 2 !== 0).reduce((a, b) => a + b);
+    } else {
+        return undefined;
+    }
 }
 
 module.exports = cubeOdd;
--- 7kyu_tests/sumOfOddCubedNums.test.js
+++ 7kyu_tests/sumOfOddCubedNums.test.js
@@ -5,10 +5,12 @@ describe("cubeOdd", () => {
     test("returns the sum of the odd numbers within an array, after cubing the initial integers", () => {
         expect(cubeOdd([1, 2, 3, 4])).toBe(28);
         expect(cubeOdd([-3, -2, 2, 3])).toBe(0);
+        expect(cubeOdd([49])).toBe(117649);
     });
 
     test("returns undefinded if any of the values aren't numbers", () => {
         expect(cubeOdd(["a", 12, 9, "z", 42])).toBe(undefined);
+        expect(cubeOdd(["1", "2", "3", "4"])).toBe(undefined);
     });
 
 });
