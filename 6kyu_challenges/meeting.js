5abbc200c28db59a5c9d5cb21b12b25a047c01a5
Genevieve Heald <gennaheald@outlook.com>
2019-12-05 22:01:08 +0000

    meeting challenge added

---
+++ 6kyu_challenges/meeting.js
@@ -0,0 +1,9 @@
+// Codewars - Meeting
+// Could you make a program that makes this string uppercase and returns it sorted in alphabetical order by last name.
+// When the last names are the same, sort them by first name. Last name and first name of a guest come in the result between parentheses separated by a comma.
+
+const meeting = s => {
+    
+}
+
+module.exports = meeting;
