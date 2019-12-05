053ad110447f42604a1950afff097554f2dded80
Genevieve Heald <gennaheald@outlook.com>
2019-12-05 16:30:04 +0000

    sortArraysIgnoreCase test amended

--- 6kyu_tests/sortArraysIgnoreCase.test.js
+++ 6kyu_tests/sortArraysIgnoreCase.test.js
@@ -5,7 +5,7 @@ describe("sortme", () => {
     test("returns sorted array in alphabetical order, ignorning case", () => {
         expect(sortme(["C", "d", "a", "B"])).toEqual(["a", "B", "C", "d"]);
         expect(sortme(["Hello", "there", "I'm", "fine"])).toEqual(["fine", "Hello", "I'm", "there"]);
-        expect(sortme(["aa", "Ab", "ac", "ad"])).toEqual(["aa", "Ab", "ac", "ad"]);
+        expect(sortme(["aa", "Ac", "aB", "ad"])).toEqual(["aa", "Ab", "ac", "ad"]);
     })
 
 })
