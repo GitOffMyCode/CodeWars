9b0350ce83e36c4d839ef668494d42b00854ba50
Genevieve Heald <gennaheald@outlook.com>
2019-12-05 22:49:42 +0000

    passing first tests

--- 6kyu_challenges/meeting.js
+++ 6kyu_challenges/meeting.js
@@ -3,7 +3,13 @@
 // When the last names are the same, sort them by first name. Last name and first name of a guest come in the result between parentheses separated by a comma.
 
 const meeting = s => {
-    
+    const names = s.toUpperCase().split(";");
+    let nameArray = [];
+    names.forEach(name => {
+        nameArray.push(name.replace(/(\w+):(\w+)/g, "($2, $1)"))
+    })
+    let result = ""
+    return result.concat(...nameArray.sort())
 }
 
 module.exports = meeting;
--- 6kyu_tests/meeting.test.js
+++ 6kyu_tests/meeting.test.js
@@ -2,8 +2,8 @@ const meeting = require("../6kyu_challenges/meeting");
 
 describe("meeting", () => {
 
-    test("returns the highest value of consonant substrings", () => {
-
+    test("returns uppercase string sorted in alphabetical order by last name", () => {
+        
         expect(meeting("Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill")).toBe("(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)");
 
         expect(meeting("Alexis:Wahl;John:Bell;Victoria:Schwarz;Abba:Dorny;Grace:Meta;Ann:Arno;Madison:STAN;Alex:Cornwell;Lewis:Kern;Megan:Stan;Alex:Korn")).toBe("(ARNO, ANN)(BELL, JOHN)(CORNWELL, ALEX)(DORNY, ABBA)(KERN, LEWIS)(KORN, ALEX)(META, GRACE)(SCHWARZ, VICTORIA)(STAN, MADISON)(STAN, MEGAN)(WAHL, ALEXIS)"); 
