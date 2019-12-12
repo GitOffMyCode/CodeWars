621389cc34a3dfdefc4d78a9177f828037f671d0
Genevieve Heald <gennaheald@outlook.com>
2019-12-12 13:36:35 +0000

    numericalsOfAString passing first tests

--- 6kyu_challenges/numericalsOfAString.js
+++ 6kyu_challenges/numericalsOfAString.js
@@ -3,9 +3,18 @@
 // "Hello, World!" -> "1112111121311"
 // "aaaaaaaaaaaa" -> "123456789101112"
 
-function numericals(s){
-    return // result
-  }
-
+const numericals = string => {
+    let charsCount = {};
+    let result = "";
+    for (let i = 0; i < string.length; i++) {
+        // if string[i] is in charsCount then add 1 to its value:
+        if (charsCount[string[i]]) charsCount[string[i]]++;
+        // otherwide add string[i] to charsCount with a value of 1:
+        else charsCount[string[i]] = 1;
+        // get the value of each char and concat to result:
+        result = result + charsCount[string[i]];  
+    }
+    return result;
+}
 
 module.exports = numericals;
