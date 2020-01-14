e65b695559ef55e0efb4092dae5439f31d56c0f8
GitOffMyCode <gennaheald@outlook.com>
2020-01-14 16:56:41 +0000

    passing 2 tests

--- 6kyu_challenges/persistentBugger.js
+++ 6kyu_challenges/persistentBugger.js
@@ -1,9 +1,18 @@
 // Codewars - Persistent Bugger
 // Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
 
-const persistence = num => {
-    console.log(num)
-
+const persistence = number => { 
+    
+    const multiplier = num => {
+        return num.toString().split('').map(num => Number(num)).reduce((a, b) => a * b)
+    }
+    let counter = 0;
+    while (number > 10) {
+        number = multiplier(number)
+        console.log(number)
+        counter++
+    } 
+    return counter
 }
 
 module.exports = persistence;
