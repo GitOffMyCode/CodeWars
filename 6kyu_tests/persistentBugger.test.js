c184cd3a14213ca34d7ee883dbba18f698ad91ba
GitOffMyCode <gennaheald@outlook.com>
2020-01-14 16:16:51 +0000

    persistent Bugger challenge added

---
+++ 6kyu_tests/persistentBugger.test.js
@@ -0,0 +1,12 @@
+const persistence = require("../6kyu_challenges/persistentBugger");
+
+describe("persistence", () => {
+
+    test("returns correct multiplicative persistence", () => {
+        expect(persistence(999)).toBe(4);
+        expect(persistence(39)).toBe(3);
+        expect(persistence(25)).toBe(2);
+        expect(persistence(4)).toBe(0);
+    })
+
+})
