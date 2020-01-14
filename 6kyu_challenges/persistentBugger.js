7b451bf442b2dc54d1d60a7739ae92450bdf0622
GitOffMyCode <gennaheald@outlook.com>
2020-01-14 16:59:08 +0000

    persistentBugger passing all tests

--- 6kyu_challenges/persistentBugger.js
+++ 6kyu_challenges/persistentBugger.js
@@ -2,14 +2,12 @@
 // Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
 
 const persistence = number => { 
-    
     const multiplier = num => {
         return num.toString().split('').map(num => Number(num)).reduce((a, b) => a * b)
     }
     let counter = 0;
-    while (number > 10) {
+    while (number > 9) {
         number = multiplier(number)
-        console.log(number)
         counter++
     } 
     return counter
--- 6kyu_tests/persistentBugger.test.js
+++ 6kyu_tests/persistentBugger.test.js
@@ -5,8 +5,8 @@ describe("persistence", () => {
     test("returns correct multiplicative persistence", () => {
         expect(persistence(999)).toBe(4);
         expect(persistence(39)).toBe(3);
-        // expect(persistence(25)).toBe(2);
-        // expect(persistence(4)).toBe(0);
+        expect(persistence(25)).toBe(2);
+        expect(persistence(4)).toBe(0);
     })
 
 })
