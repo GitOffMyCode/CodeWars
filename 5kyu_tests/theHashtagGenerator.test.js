a97b9fb9092b5a488fdd21fbde10b0605de2d1e1
GitOffMyCode <gennaheald@outlook.com>
2020-02-13 10:29:34 +0000

    hashtag generator challenge added

---
+++ 5kyu_tests/theHashtagGenerator.test.js
@@ -0,0 +1,35 @@
+const generateHashtag = require("../5kyu_tests/generateHashtag.test.js");
+
+describe("generateHashtag", () => {
+
+  test("returns false if string has no alphabetic characters", () => {
+    expect(generateHashtag("")).toBe(false);
+    expect(generateHashtag(" ")).toBe(false);
+    expect(generateHashtag(" $ ^ ? / % & . ! ^ ( * # ")).toBe(false);
+  }) 
+
+  test("handles a single word and ignores spaces", () => {
+    expect(generateHashtag("code")).toBe("#Code");
+    expect(generateHashtag(" love ")).toBe("#Love");
+  }) 
+
+  test("handles single characters", () => {
+    expect(generateHashtag("a b c")).toBe("#ABC");
+    expect(generateHashtag(" a before z ")).toBe("#ABeforeZ");
+  }) 
+
+  test("handles multiple words", () => {
+    expect(generateHashtag("love coding")).toBe("#LoveCoding");
+    expect(generateHashtag(" dance like no one is watching ")).toBe("#DanceLikeNoOneIsWatching");
+  }) 
+
+  test("ignores non alphabetic characters", () => {
+    expect(generateHashtag("love coding!")).toBe("#LoveCoding");
+    expect(generateHashtag(" !@£$%^&*() buzz off ")).toBe("#BuzzOff");
+  }) 
+
+  test("returns false if result is longer than 140 characters", () => {
+    expect(generateHashtag("tooooooooo oooooooooo oooooooooo oooooooooo oooooooooo oooooooooo oooooooooo oooooooooo oooooooooo oooooooooo oooooooooo oooooooooo oooooooooo oooooooooo long")).toBe(false);
+  }) 
+
+})
